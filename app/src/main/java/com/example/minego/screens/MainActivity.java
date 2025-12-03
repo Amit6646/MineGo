4288c1773bfd881d1044c797c4b832b81a17c9c3
Student-2024 <student-2024@ashalim.org>
2025-12-03 08:30:22 +0200

    logout button + start working on map

--- app/src/main/java/com/example/minego/screens/MainActivity.java
+++ app/src/main/java/com/example/minego/screens/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.minego.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.minego.R;
+import com.example.minego.utils.SharedPreferencesUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnLogout = findViewById(R.id.btn_main_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferencesUtil.signOutUser(MainActivity.this);
+                Intent mainIntent = new Intent(MainActivity.this, LandingActivity.class);
+                /// Clear the back stack (clear history) and start the MainActivity
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(mainIntent);
+            }
+        });
+
+
+        // https://github.com/osmdroid/osmdroid/wiki/How-to-use-the-osmdroid-library-(Java)
     }
 }
